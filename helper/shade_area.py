5e6455ab6c5dfd904a917efe4924cfaaa9aa80d2
Dominic <dominic_ong@mymail.sutd.edu.sg>
2018-04-11 00:06:46 +0800

    Changed highlight to highlight in yellow instead of box highlight, and added automatic stride calculation

--- helper/shade_area.py
+++ helper/shade_area.py
@@ -1,11 +1,12 @@
 import numpy as np
 
-def shade_area(img, result, window_size, stride, line_size = 5):
+def shade_area(img, imgs, result, window_size, stride, thickness = (8, 8)):
 	boxed = np.array(img)
-	X, Y = img.shape
+	X, Y = img.shape[0], img.shape[1]
 	sX, sY = stride
 	wX, wY = window_size
 	nX, nY = result.shape
+	pW, pH = thickness
 	
 	for i in range(nX):
 		for j in range(nY):
@@ -13,6 +14,34 @@ def shade_area(img, result, window_size, stride, line_size = 5):
 				x = i * sX
 				y = j * sY
 				
-				boxed[x:x+wX, y:y+wY] = img[x:x+wX, y:y+wY] * 0.75
-	
+				mask = (imgs[i, j] != 0.0)
+				mask2 = np.array(boxed[x:x+wX, y:y+wY])
+				
+				for offsetX in range(-pW, pW+1):
+					for offsetY in range(-pH, pH+1):
+						
+						minX = max(0, 0 + offsetX)
+						maxX = min(wX, wX + offsetX)
+						
+						minY = max(0, 0 + offsetY)
+						maxY = min(wY, wY + offsetY)
+						
+						m = mask[minX:maxX, minY:maxY]
+						
+						xpad = np.zeros(shape = (abs(offsetX), maxY-minY), dtype = bool)
+						if offsetX > 0:
+							m = np.append(m, xpad, axis = 0)
+						else:
+							m = np.append(xpad, m, axis = 0)
+						
+						ypad = np.zeros(shape = (wX, abs(offsetY)), dtype = bool)
+						if offsetY > 0:
+							m = np.append(m, ypad, axis = 1)
+						else:
+							m = np.append(ypad, m, axis = 1)
+						
+						mask2[m] = [255, 255, 0]
+				
+				boxed[x:x+wX, y:y+wY] = mask2
+				
 	return boxed
