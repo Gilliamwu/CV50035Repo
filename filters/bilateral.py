1f373b865a8927c56e882d3b46a86dfd6ec6fcc8
Jinglianwu <ntjinglian@gmail.com>
2018-04-11 03:02:54 +0800

    update, add adaptive thresholding

--- filters/bilateral.py
+++ filters/bilateral.py
@@ -1,17 +1,17 @@
 import cv2
 import numpy as np
-from .filter import GrayscaleNormalizer
+#from .filter import GrayscaleNormalizer
 
 from sklearn import linear_model
 
 class bilateral_canny:
 	def __init__(self):
-		self.normalizer = GrayscaleNormalizer()
+		#self.normalizer = GrayscaleNormalizer()
+		pass
 
 	def bilateral_canny_img(self, img):
-	    img2 = self.normalizer(img)
-	    img2 = np.uint8(img2)
-	    blur = cv2.bilateralFilter(img2,10,20,20)
+	    img = np.uint8(img)
+	    blur = cv2.bilateralFilter(img,10,20,20)
 	    blur = np.uint8(blur)
 	    myfiltered = cv2.Canny(blur,100,200)
 	    #myfiltered = myfiltered.reshape(-1)
