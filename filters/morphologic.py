ab57024cc4b0d94fc8866f166db45f204dc50c28
Dominic <dominic_ong@mymail.sutd.edu.sg>
2018-04-16 19:49:27 +0800

    Added filter preview notebook, fixed some filter code

--- filters/morphologic.py
+++ filters/morphologic.py
@@ -2,12 +2,10 @@ import cv2
 import numpy as np
 
 def tophat( img, element=(5, 5) ):
-	kernel = cv2.getStructuringElement(cv2.MORPH_CROSS,element)
-	return cv2_morphology( img, cv2.MORPH_TOPHAT, kernel )
+	return cv2_morphology( img, cv2.MORPH_TOPHAT, element )
 
 def blackhat( img, element = (5, 5) ):
-	kernel = cv2.getStructuringElement(cv2.MORPH_CROSS,element)
-	return cv2_morphology( img, cv2.MORPH_BLACKHAT, kernel )
+	return cv2_morphology( img, cv2.MORPH_BLACKHAT, element )
 	
 def gradient( img, element = (5, 5) ):
 	return cv2_morphology( img, cv2.MORPH_GRADIENT, element )
@@ -19,9 +17,7 @@ def gradient2( img ):
 	kernel2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
 	
 	gradient1 = cv2.morphologyEx(img, cv2.MORPH_GRADIENT, kernel1)
-	gradient1 = gradient1.reshape(-1)
 	gradient2 = cv2.morphologyEx(img, cv2.MORPH_GRADIENT, kernel2)
-	gradient2 = gradient2.reshape(-1)
 	
 	myfiltered = np.multiply(gradient1, gradient2)
 	
@@ -36,11 +32,8 @@ def gradient3( img ):
 	kernel3 = cv2.getStructuringElement(cv2.MORPH_CROSS,(5,5))
 	
 	gradient1 = cv2.morphologyEx(img, cv2.MORPH_GRADIENT, kernel1)
-	gradient1 = gradient1.reshape(-1)
 	gradient2 = cv2.morphologyEx(img, cv2.MORPH_GRADIENT, kernel2)
-	gradient2 = gradient2.reshape(-1)
 	gradient3 = cv2.morphologyEx(img, cv2.MORPH_GRADIENT, kernel3)
-	gradient3 = gradient3.reshape(-1)
 	
 	myfiltered = np.multiply(gradient1, gradient2)
 	myfiltered = np.multiply(gradient3, myfiltered)
@@ -54,11 +47,9 @@ def canny_gradient( img ):
 	blur = np.uint8(blur)
 	
 	mycanny = cv2.Canny(blur,100,200)
-	mycanny = mycanny.reshape(-1)
 	
 	kernel = cv2.getStructuringElement(cv2.MORPH_CROSS,(5,5))
 	mygrad = cv2.morphologyEx(img, cv2.MORPH_BLACKHAT, kernel)
-	mygrad = mygrad.reshape(-1)
 	myfiltered = np.multiply(mygrad,mycanny)
 	return myfiltered
 
--- filters/otsu.py
+++ filters/otsu.py
@@ -5,6 +5,8 @@ def otsu( img, element = (5,5) ):
     img = np.uint8(img)
     blur = cv2.GaussianBlur(img,element,0)
     ret3,myfiltered = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-    myfiltered = np.subtract(myfiltered,255)
-    myfiltered = np.multiply(myfiltered, -1)
+    myfiltered -= 255
+    myfiltered = np.abs(myfiltered)
+    #myfiltered = np.subtract(myfiltered,255)
+    #myfiltered = np.multiply(myfiltered, -1)
     return myfiltered
