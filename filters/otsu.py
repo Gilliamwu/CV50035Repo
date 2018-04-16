b4d222185cc23e52e73967fd2c645461a1cf3ec0
shaunyhy <shaunyhy@gmail.com>
2018-04-16 19:12:05 +0800

    changed otsu to reverse color

--- filters/otsu.py
+++ filters/otsu.py
@@ -5,4 +5,6 @@ def otsu( img, element = (5,5) ):
     img = np.uint8(img)
     blur = cv2.GaussianBlur(img,element,0)
     ret3,myfiltered = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    myfiltered = np.subtract(myfiltered,255)
+    myfiltered = np.multiply(myfiltered, -1)
     return myfiltered
